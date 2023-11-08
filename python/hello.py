81d5d79855c331573a0c1ff255fb58b3cd26c1b3
Maxamed Maxamed <20088349@mail.wit.ie>
2023-11-08 19:19:42 +0000

    first time

--- python/hello.py
+++ python/hello.py
@@ -1,13 +1,15 @@
+# Get the user's name
+name = input("What's your name? ")
 
-print("Hello, World!")
-print("hello world!!!!")
-print("Hello, World!")
-print("Hello, World!")
-print("Hello, World!")
-print("Hello, World!")
-print("Hello, World!")
-print("hello world!!!!")
-print("Hello, World!")
-print("Hello, World!")
-print("Hello, World!")
-print("Hello, World!")
+# Get information about what the user does for a living
+occupation = input("What do you do for a living? ")
+
+# Print the collected information
+print(f"Hello, {name}! It's nice to meet you.")
+print(f"So, you are a {occupation}. That's interesting!")
+
+# Optionally, you can save this information to a file for later use
+with open("user_info.txt", "w") as file:
+    file.write(f"Name: {name}\nOccupation: {occupation}")
+
+# Close the program
